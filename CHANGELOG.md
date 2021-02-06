cf4c2ea675342f33efd54854cf480bebc7fe1329
Rick <kmasnaveev@gmail.com>
2021-02-06 17:51:05 +0300

    Update 1.2

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+<h1 align="center">Changelog</h1>
+
+# *Last Update ->* **1.2**
+## Update 1.2
+### Bug fixes
+- If program cant find streamers.txt program throw error
+- Skiping streamer if ?referrer=raid in url
+### Features
+- Now program claim your reward
+- Streamer removed from config after receiving reward
+## Update 1.1
+### Bug fixes
+- Now getting percent working correctly
--- drops.py
+++ drops.py
@@ -30,8 +30,33 @@ def check_online():
     elif offline == False:
         return True
 
+def delete_streamer(streamer):
+    Streamers.remove(streamer)
+    try:
+        with open("streamers.txt", "r") as f:
+            lines = f.readlines()
+        with open("streamers.txt", "w") as f:
+            for line in lines:
+                if line.strip("\n") != streamer:
+                    f.write(line)
+    except:
+        pass
+
+
 RETURN = 0
 LAST_PERCENT = 0
+
+def claim_reward(streamer):
+    driver.get('https://www.twitch.tv/drops/inventory')
+    if check_exists_by_xpath("//*[contains(text(), 'Claim Now')]") == False:
+        return
+    button = driver.find_element_by_xpath("//*[contains(text(), 'Claim Now')]")
+    button.click()
+    driver.get(streamer)
+    return
+
+    
+    
 
 def get_percent():
     global LAST_PERCENT
@@ -39,9 +64,12 @@ def get_percent():
     try:
         percent = driver.find_element_by_xpath("/html/body/div[5]/div/div/div/div/div/div/div/div/div/div/div/div[3]/div/div/div[1]/div[9]/a/div/div[2]/p[2]")
         percent = percent.text
-        percent = percent[:4]
-        percent = percent.replace('%', '')
-        percent = percent.replace(' ', '')
+        percent = percent[:3]
+        try:
+            percent = percent.replace('%', '')
+            percent = percent.replace(' ', '')
+        except:
+            pass
         percent = int(percent)
     except:
         percent = 1
@@ -53,21 +81,11 @@ def get_percent():
     else:
         RETURN = 0
     LAST_PERCENT = percent
-    if RETURN == 11:
+    if RETURN == 5:
         percent = 100
-        RETURN = 0
     return percent
 
-Streamers = [
-    'https://www.twitch.tv/juansguarnizo',
-    'https://www.twitch.tv/Mizkif',
-    'https://www.twitch.tv/IlloJuan',
-    'https://www.twitch.tv/moistcr1tikal',
-    'https://www.twitch.tv/sodapoppin',
-    'https://www.twitch.tv/Alexby11',
-    'https://www.twitch.tv/Rubius',
-    'https://www.twitch.tv/aroyitt'
-]
+Streamers = []
 
 try:
     with open('streamers.txt', "r") as override_config:
@@ -79,14 +97,16 @@ try:
         override_config.close()
     print(f"Config overrided by streamers.txt!")
     for streamer_url in Streamers:
-            print(streamer_url)
+        print(streamer_url)
 except:
-    pass
+    print("streamers.txt not found. Create streamers.txt and fill urls to watch")
+    os._exit(0)
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--mute-audio")
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36")
 driver = webdriver.Chrome(file, chrome_options=chrome_options)
+
 
 driver.get('https://www.twitch.tv/login')
 driver.implicitly_wait(5)
@@ -99,25 +119,36 @@ while(True):
     for element in Streamers:
         driver.get(element)
         online = check_online()
-        if online:
-            print(f"{element} is online")
-            mature = check_exists_by_xpath('//*[@id="root"]/div/div[2]/div/main/div[2]/div[3]/div/div/div[3]/div/div[2]/div/div/div/div[5]/div/div[3]/button')
-            if (mature):
-                button = driver.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/main/div[2]/div[3]/div/div/div[3]/div/div[2]/div/div/div/div[5]/div/div[3]/button')
-                button.click()
-            button = driver.find_element_by_xpath('//*[@id="root"]/div/div[2]/nav/div/div[3]/div[6]/div/div/div/div/button')
-            button.click()
-            if check_exists_by_xpath("//*[contains(text(), 'Drops Enabled')]"):
-                pass
-            else:
-                continue
-            while(True):
-                percent = get_percent()
-                print(percent)
-                if percent < 99:
-                    sleep(72)
-                if (percent) >= 99:
-                    sleep(130)
-                    break
-        elif online == False:
+        if online == False:
             print(f"{element} is offline")
+            continue
+
+        print(f"{element} is online")
+        if check_exists_by_xpath("//*[contains(text(), 'Drops Enabled')]") == False:
+            print("Streaming but drops not enabled!")
+            continue
+
+        mature = check_exists_by_xpath('//*[@id="root"]/div/div[2]/div/main/div[2]/div[3]/div/div/div[3]/div/div[2]/div/div/div/div[5]/div/div[3]/button')
+        if (mature):
+            button = driver.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/main/div[2]/div[3]/div/div/div[3]/div/div[2]/div/div/div/div[5]/div/div[3]/button')
+            button.click()
+
+        current_url = str(driver.current_url)
+        if current_url.find('?referrer=raid') != -1:
+            continue
+
+        button = driver.find_element_by_xpath('//*[@id="root"]/div/div[2]/nav/div/div[3]/div[6]/div/div/div/div/button')
+        button.click()
+        while(True):
+            percent = get_percent()
+            print(percent)
+            if percent < 99:
+                sleep(72)
+            if (percent) >= 99:
+                sleep(130)
+                if RETURN == 0:
+                    delete_streamer(element)
+                    claim_reward(element)
+                RETURN = 0
+                break
+            
